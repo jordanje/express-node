bd91c2b7204ef265741f6daa49cbb479904810d8
Jordan Jennings <jordanjennings12@gmail.com>
2022-01-26 21:41:51 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/currenttime", function (req, res) {
+  res.send("<h1>" + new Date().toISOString() + "<h1>");
+}); //localhost:3000/currenttime
+
+app.get("/", function (req, res) {
+  res.send(
+    "<form action='/store-user' method='POST'><label>Your Name</label><input type='text' name='username'><button>Submit</button></form>"
+  );
+});
+
+app.post("/store-user", function (req, res) {
+  const userName = req.body.username;
+  const filePath = path.join(__dirname, "data", "users.json");
+
+  const fileData = fs.readFileSync(filePath);
+
+  const existingUsers = JSON.parse(fileData);
+  existingUsers.push(userName);
+
+  fs.writeFileSync(filePath, JSON.stringify(existingUsers));
+
+  res.send("<h1>Username stored!</h1>");
+});
+
+//app.listen(3000);
+
+/*
+function handleRequest(request, response) {
+  if (request.url === "/currenttime") {
+    response.statusCode = 200;
+    response.end("<h1>" + new Date().toISOSString() + "<h1>");
+  } else if (request.url === "/") {
+    response.statusCode = 200;
+    response.end("<h1>Hello World<h1>");
+  }
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(3000); */
+
+module.exports = app;
